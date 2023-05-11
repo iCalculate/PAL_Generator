5d402b94716ac4e05bd2d782e283915a5b7f04f7
ChauncyDu <133197063+ChauncyDu@users.noreply.github.com>
2023-05-11 14:58:53 +0800

    Update PALGen.py

--- PALGen.py
+++ PALGen.py
@@ -7,8 +7,8 @@ import numpy as np
 def get_color(x, y):
 	gdi32 = windll.gdi32
 	user32 = windll.user32
-	hdc = user32.GetDC(None)  # 获取颜色值
-	pixel = gdi32.GetPixel(hdc, x, y)  # 提取RGB值
+	hdc = user32.GetDC(None)  # Get the color value
+	pixel = gdi32.GetPixel(hdc, x, y)  # Get pixel RGB value
 	r = pixel & 0x0000ff
 	g = (pixel & 0x00ff00) >> 8
 	b = pixel >> 16
